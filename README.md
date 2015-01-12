64e552146ee1595f087e8b658ec83580ee3d3371
Erik Dubbelboer <erik@dubbelboer.com>
2015-01-12 08:13:37 +0000

    Fixed TODO, changed bind

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 TODO
 ----
 
-* Don't always broadcast all counters.
+* dcounter cli members
 * https://github.com/codegangsta/cli
 * https://github.com/boltdb/bolt as backend store
 
--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"net"
+	"strconv"
 	"syscall"
 
 	dc "github.com/atomx/dcounter/server"
@@ -25,17 +27,27 @@ func (h *hosts) Set(value string) error {
 func server(arguments []string) {
 	flags := flag.NewFlagSet("server", flag.ExitOnError)
 	name := flags.String("name", "", "Name for this instance")
-	bind := flags.String("bind", "127.0.0.1:9373", "Sets the bind address for cluster communication")
+	bindAddr := flags.String("bind-addr", "127.0.0.1:9373", "Sets the bind address for cluster communication")
+	bindPort := flags.Int("bind-port", 9373, "Sets the bind port address for cluster communication")
 	client := flags.String("client", "127.0.0.1:9374", "Sets the address to bind for client access")
 	join := make(hosts, 0)
 	flags.Var(&join, "join", "Join these hosts after starting")
 	flags.Parse(arguments)
 
 	if *name == "" {
-		*name = *bind
+		*name = *bindAddr
 	}
 
-	s := dc.New(*name, *bind, *client)
+	if ips, err := net.LookupIP(*bindAddr); err != nil {
+		log.Printf("[ERR] %v", err)
+		return
+	} else {
+		*bindAddr = ips[0].String()
+	}
+
+	bind := *bindAddr + ":" + strconv.FormatInt(int64(*bindPort), 10)
+
+	s := dc.New(*name, bind, *client)
 
 	go func() {
 		s.Start()
